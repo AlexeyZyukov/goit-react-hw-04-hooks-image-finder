bea6facb206488eebe38c216a0dde104e8fbf4e9
Alexey Zyukov <aalex64@ukr.net>
2021-12-21 22:04:07 +0200

    refactored Modal

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,36 +1,26 @@
-import React, { useEffect, Component } from 'react';
-import PropTypes, { string } from 'prop-types';
+import React, { useEffect } from 'react';
 import styles from './modal.module.css';
 
-export default class Modal extends Component {
-  static = {
-    onToggleModal: PropTypes.func.isRequired,
-    closeModal: PropTypes.func.isRequired,
-    image: string,
-  };
+export default function Modal(props) {
+  useEffect(() => {
+    window.addEventListener('keydown', handleEscClick);
+    return () => {
+      window.removeEventListener('keydown', handleEscClick);
+    };
+  });
 
-  componentDidMount() {
-    window.addEventListener('keydown', this.handleEscClick);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('keydown', this.handleEscClick);
-  }
-
-  handleEscClick = evt => {
+  const handleEscClick = evt => {
     if (evt.code === 'Escape') {
-      this.props.closeModal();
+      props.closeModal();
     }
   };
 
-  render() {
-    const { onToggleModal, image } = this.props;
-    return (
-      <div className={styles.overlay} onClick={onToggleModal} tabIndex="0">
-        <div className={styles.modal}>
-          <img src={image} alt={image.tags} />
-        </div>
+  const { onToggleModal, image } = props;
+  return (
+    <div className={styles.overlay} onClick={onToggleModal} tabIndex="0">
+      <div className={styles.modal}>
+        <img src={image} alt={image.tags} />
       </div>
-    );
-  }
+    </div>
+  );
 }
